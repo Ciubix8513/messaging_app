afa60e29a1fd430ee14cf13a0692252bf45552c7
Luna <ciubix8514@gmail.com>
2023-05-09 23:37:33 +0300

    Add endpoint url static variables

---
+++ frontend/src/grimoire.rs
@@ -0,0 +1,19 @@
+use once_cell::sync::Lazy;
+use reqwest::Url;
+
+pub static API_URL: Lazy<Url> = Lazy::new(|| Url::parse("http://10.69.69.119:8080").unwrap());
+
+pub static AUTH_LOGIN: Lazy<Url> = Lazy::new(|| API_URL.join("/auth/login").unwrap());
+pub static AUTH_LOGOUT: Lazy<Url> = Lazy::new(|| API_URL.join("/auth/logout").unwrap());
+pub static AUTH_CHANGE_PASSWORD: Lazy<Url> =
+    Lazy::new(|| API_URL.join("/auth/change-password").unwrap());
+pub static CHATS_CREATE: Lazy<Url> = Lazy::new(|| API_URL.join("/chats/create").unwrap());
+pub static CHATS_EXIT: Lazy<Url> = Lazy::new(|| API_URL.join("/chats/exit").unwrap());
+pub static CHATS_GET: Lazy<Url> = Lazy::new(|| API_URL.join("/chat/get").unwrap());
+pub static INVITES_SEND: Lazy<Url> = Lazy::new(|| API_URL.join("/invites/send").unwrap());
+pub static INVITES_GET: Lazy<Url> = Lazy::new(|| API_URL.join("/invites/get").unwrap());
+pub static INVITES_REJECT: Lazy<Url> = Lazy::new(|| API_URL.join("/invites/reject").unwrap());
+pub static INVITES_ACCEPT: Lazy<Url> = Lazy::new(|| API_URL.join("/invites/accept").unwrap());
+pub static MESSAGES_SEND: Lazy<Url> = Lazy::new(|| API_URL.join("/messages/send").unwrap());
+pub static MESSAGES_GET: Lazy<Url> = Lazy::new(|| API_URL.join("/messages/get").unwrap());
+pub static USERS_ADD_USER: Lazy<Url> = Lazy::new(|| API_URL.join("/users/add-user").unwrap());
